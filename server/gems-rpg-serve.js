967e2efdbd712802ad982633f03955104cb8ee13
方珂琛 <984512789@qq.com>
2022-01-18 21:00:05 +0800

    修复服务端bug

--- server/gems-rpg-serve.js
+++ server/gems-rpg-serve.js
@@ -58,113 +58,118 @@
     console.log(msg);
     fs.appendFile(path, msg + "\r\n", "utf8", () => void 0);
   }
-
-  const clients = new Map();
-  function singleSend(connection, payload) {
-    connection.write(JSON.stringify(payload));
-  }
-  function singlecast(payload) {
-    if (clients.has(payload.target)) {
-      singleSend(clients.get(payload.target).connect, payload);
+  try {
+    const clients = new Map();
+    function singleSend(connection, payload) {
+      connection.write(JSON.stringify(payload));
     }
-  }
-  function broadcast(payload) {
-    clients.forEach(({ connect }) => {
-      singleSend(connect, payload);
-    });
-  }
-  const onData = {
-    sys(data) {
-      switch (data.type) {
-        case "login": {
-          const { account, password } = data.data;
-          if (clients.has(account)) {
-            singleSend(this, {
-              type: "sys",
-              data: { type: "login", msg: "该账号已登录", code: 1 },
-            });
-            printLog(`用户 ${account} 试图重复登录，当前在线${clients.size}人`);
-            return;
-          }
-          const model = readUser(account);
-          if (model) {
-            if (model.password !== password) {
+    function singlecast(payload) {
+      if (clients.has(payload.target)) {
+        singleSend(clients.get(payload.target).connect, payload);
+      }
+    }
+    function broadcast(payload) {
+      clients.forEach(({ connect }) => {
+        singleSend(connect, payload);
+      });
+    }
+    const onData = {
+      sys(data) {
+        switch (data.type) {
+          case "login": {
+            const { account, password } = data.data;
+            if (clients.has(account)) {
               singleSend(this, {
                 type: "sys",
-                data: { type: "login", msg: "密码错误", code: 1 },
+                data: { type: "login", msg: "该账号已登录", code: 1 },
               });
-              printLog(`用户 ${account} 登录失败，当前在线${clients.size}人`);
+              printLog(
+                `用户 ${account} 试图重复登录，当前在线${clients.size}人`
+              );
               return;
             }
+            const model = readUser(account);
+            if (model) {
+              if (model.password !== password) {
+                singleSend(this, {
+                  type: "sys",
+                  data: { type: "login", msg: "密码错误", code: 1 },
+                });
+                printLog(`用户 ${account} 登录失败，当前在线${clients.size}人`);
+                return;
+              }
+            }
+            writeUser(account, { account, password, loginTime: Date.now() });
+            const gameData = { account, password, ...model };
+            singleSend(this, {
+              type: "sys",
+              data: { type: "login", msg: "登录成功", code: 0, data: gameData },
+            });
+            this.account = account;
+            clients.set(account, { connect: this, data: gameData });
+            printLog(`用户 ${account} 登录成功，当前在线${clients.size}人`);
+            onData.sys.call(this, { type: "getPlayers" });
+            return;
           }
-          writeUser(account, { account, password, loginTime: Date.now() });
-          const gameData = { account, password, ...model };
-          singleSend(this, {
-            type: "sys",
-            data: { type: "login", msg: "登录成功", code: 0, data: gameData },
-          });
-          this.account = account;
-          clients.set(account, { connect: this, data: gameData });
-          printLog(`用户 ${account} 登录成功，当前在线${clients.size}人`);
-          onData.sys.call(this, { type: "getPlayers" });
-          return;
+          case "getPlayers": {
+            broadcast({
+              type: "sys",
+              data: {
+                type: "getPlayers",
+                code: 0,
+                msg: "同步在线用户",
+                data: Array.from(clients, ([_, value]) => {
+                  return value.data.account;
+                }),
+              },
+            });
+            return;
+          }
         }
-        case "getPlayers": {
-          broadcast({
-            type: "sys",
-            data: {
-              type: "getPlayers",
-              code: 0,
-              msg: "同步在线用户",
-              data: Array.from(clients, ([_, value]) => {
-                return value.data.account;
-              }),
-            },
-          });
-          return;
-        }
-      }
-      // if (data.type === "login") {
+        // if (data.type === "login") {
 
-      // }
-    },
-    single(data, target, source, payload) {
-      switch (data.type) {
-        case "chat": {
-          printLog(`用户 ${source} 对 ${target} 发起私聊 内容:${data.data}`);
-          singlecast(payload);
-          return;
+        // }
+      },
+      single(data, target, source, payload) {
+        switch (data.type) {
+          case "chat": {
+            printLog(`用户 ${source} 对 ${target} 发起私聊 内容:${data.data}`);
+            singlecast(payload);
+            return;
+          }
         }
-      }
-    },
-  };
-  net
-    .createServer((connection) => {
-      printLog("创建连接了");
-      connection.on("data", (dto) => {
-        printLog(dto);
-        const payload = JSON.parse(dto);
-        const { type, data, target, source } = payload;
-        const fn = onData[type];
-        fn && fn.call(connection, data, target, source, payload);
-      });
-      connection.on("close", () => {
-        printLog("close 断开连接了");
-        const { account } = connection;
-        if (clients.has(account)) {
-          writeUser(account, clients.get(account).data);
-          clients.delete(account);
-          printLog(`用户 ${account} 断开连接，当前在线${clients.size}人`);
-        }
-        connection.end();
-        onData.sys.call(connection, { type: "getPlayers" });
-      });
+      },
+    };
+    net
+      .createServer((connection) => {
+        printLog("创建连接了");
+        connection.on("data", (dto) => {
+          printLog(dto);
+          const payload = JSON.parse(dto.toString());
+          const { type, data, target, source } = payload;
+          const fn = onData[type];
+          fn && fn.call(connection, data, target, source, payload);
+        });
+        connection.on("close", () => {
+          printLog("close 断开连接了");
+          const { account } = connection;
+          if (clients.has(account)) {
+            writeUser(account, clients.get(account).data);
+            clients.delete(account);
+            printLog(`用户 ${account} 断开连接，当前在线${clients.size}人`);
+          }
+          connection.end();
+          onData.sys.call(connection, { type: "getPlayers" });
+        });
 
-      connection.on("error", (err) => {
-        printLog(`用户 ${connection.account} 强制断开连接`);
-        printLog(err);
-        connection.end();
-      });
-    })
-    .listen(PORT, () => printLog(`服务运行至 http://${ip}:${PORT}`));
+        connection.on("error", (err) => {
+          printLog(`用户 ${connection.account} 强制断开连接`);
+          printLog(err);
+          connection.end();
+        });
+      })
+      .listen(PORT, () => printLog(`服务运行至 http://${ip}:${PORT}`));
+  } catch (error) {
+    printLog(error);
+  }
 })();
